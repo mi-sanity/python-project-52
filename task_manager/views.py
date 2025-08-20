e3c1b39b9bbd94db448010afac3a31ca0e892401
Olesia Pankova <pankovaolesya77@yandex.ru>
2025-08-20 21:22:27 +0400

    edit class IndexView

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic.base import TemplateView
 
 
-class IndexView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name = 'index.html'
