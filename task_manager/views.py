9f8e405d1c51689fcbe9f017487bc2ad0e1d6360
Olesia Pankova <pankovaolesya77@yandex.ru>
2025-08-20 21:46:51 +0400

    edit IndexView, add django_bootstrap5

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views import View
 
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
