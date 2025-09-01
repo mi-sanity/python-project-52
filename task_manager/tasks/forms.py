4a3c0a256ee76c548ff57ec5942b838bc2d943b7
Olesia Pankova <pankovaolesya77@yandex.ru>
2025-09-01 14:43:25 +0400

    add tasks

---
+++ task_manager/tasks/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from task_manager.statuses.models import Status
+from task_manager.tasks.models import Task
+from task_manager.users.models import User
+
+
+class TaskForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["executor"].queryset = User.objects.all()
+        self.fields["executor"].label_from_instance = (
+            lambda obj: obj.get_full_name()
+        )
+
+    class Meta:
+        model = Task
+        fields = ("name", "description", "status", "executor")
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": _("Name"),
+                }
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": _("Description"),
+                }
+            ),
+            "status": forms.Select(
+                attrs={
+                    "class": "form-control",
+                    "choices": Status,
+                }
+            ),
+            "executor": forms.Select(
+                attrs={
+                    "class": "form-control",
+                    "choices": User,
+                }
+            ),
+        }
+        labels = {
+            "name": _("Name"),
+            "description": _("Description"),
+            "status": _("Status"),
+            "executor": _("Executor"),
+        }
