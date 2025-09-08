4282c979dc611c86f95f3c28ab8083aee02161fa
Olesia Pankova <pankovaolesya77@yandex.ru>
2025-09-08 14:44:59 +0400

    add sonarcloud

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 [![Actions Status](https://github.com/mi-sanity/python-project-52/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/mi-sanity/python-project-52/actions)
 
 
+### SonarQube status:
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=mi-sanity_python-project-52&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=mi-sanity_python-project-52)
+
+
 ### Domain link:
 https://task-manager-8d90.onrender.com
 
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -17,7 +17,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-96l*y#h((c=)-#ce2#mud7vg510_86ogh1c&6g4%2tjq0^-l$0'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
